841305d22fa3bdecd0c57721708de312498f5ac1
eiengen <engen@Engen2018.local>
2020-11-22 19:05:48 +0100

    constant KF gain

--- MatlabConfigFiles/linearize_script.m
+++ MatlabConfigFiles/linearize_script.m
@@ -4,8 +4,10 @@
 % Define model parameters
 system_parameters
 % Operating points for the states
-th_op    = -10*pi/180; yw_op    = pi/2;
-w_th_op  = 0;          w_yw_op  = 0;
+th_op    = -10*pi/180; 
+yw_op    = pi/2;
+w_th_op  = 0;          
+w_yw_op  = 0;
 % Operating points for the inputs
 Vmp_op = (Kyy*m_heli*g*cos(th_op)*l_cm) / (Kyy*Kpp - Kyp*Kpy);
 Vmy_op = (Kyp*Vmp_op) / (Kyy);
---
+++ task(6)_Sim_StateEstimator/initialize_system.m
@@ -0,0 +1,43 @@
+
+clearvars
+clc
+
+% Define model parameters
+heli_parameters
+% Operating points for the states
+th_op    = -30*pi/180; 
+yw_op    = pi/2;
+w_th_op  = 0;          
+w_yw_op  = 0;
+% Operating points for the inputs
+Vmp_op = (Kyy*m_heli*g*cos(th_op)*l_cm) / (Kyy*Kpp - Kyp*Kpy);
+Vmy_op = (Kyp*Vmp_op) / (Kyy);
+
+% Compute linearized Ac, Bc and Cc
+linearize_heli
+sys = ss(Ac,Bc,Cc,0);
+ds = c2d(sys,0.1);
+A = ds.A;
+B = ds.B;
+C = ds.C;
+
+% Calculate the steady state Kalman filter gain offline
+%for measurment noise transmission matrix
+G = eye(4); %this is difficult to know in reality. So assume it is identity matrix (i.e. 1)
+H = zeros(2,4);
+%weighting matrices for process noise and measurement noise
+%plant model (knowledge about the process)
+% dx = Ax + Bu + Gw
+% y_plant = Cx + v
+% w and v are process and measurment noises respectively
+%plant = ss(A,[B,G],C,[D H]); %D = 0 since it is not present
+plant = ss(Ac,[Bc,G],Cc,0); %D = 0 since it is not present
+%%
+Q = diag([1e1,7e1,4e1,6e1]); %process
+R = diag([0.01,0.1]); %measurement noise
+%note: If you trust your sensors, you can put relatively less R. If you do
+%not trust your model, you can put relatively more Q.
+
+%calculate the initial kalman gain L
+[kalmf,L,P,M] = kalman(plant,Q,R);
+ 
